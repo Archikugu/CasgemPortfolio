2ee6b08f9e86bc82197044ce549183bc37ee6a4a
Gokhan <engokhangok@gmail.com>
2023-07-12 04:04:04 +0300

    Referanslar UI Eklendi

--- CasgemPortfolio/Controllers/ReferenceController.cs
+++ CasgemPortfolio/Controllers/ReferenceController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CasgemPortfolio.Models.Entities;
 
 namespace CasgemPortfolio.Controllers
 {
     public class ReferenceController : Controller
     {
+        CasgemPortfolioDbEntities db = new CasgemPortfolioDbEntities();
+
         public ActionResult Index()
         {
             return View();
